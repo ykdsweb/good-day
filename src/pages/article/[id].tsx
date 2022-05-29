965bdab24ef0ddd3d8b3ab87d81788408f2f8d93
Tatsuya1117 <othe.world.pc@gmail.com>
2022-05-29 15:25:14 +0900

    firstcommit

---
+++ src/pages/article/[id].tsx
@@ -0,0 +1,55 @@
+import { GetServerSideProps } from 'next';
+import { client } from '../../../libs/client';
+import styles from '../../styles/Home.module.scss';
+import type { Article } from '../../../types/article';
+
+type Props = {
+  article: Article;
+};
+
+export default function Article({ article }: Props) {
+  return (
+    <div className="">
+      <div className="">
+        <div className="">
+          <img
+            className=""
+            src={article.eye_catch.url}
+          />
+          <div className="">
+            <div className="">
+              {article.title}
+            </div>
+          </div>
+          {article.tag && (
+            <div className="">
+              <div className="">
+                #{article.tag}
+              </div>
+            </div>
+          )}
+          <div className="">
+            <div className="">
+              {article.body}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const id = ctx.params?.id;
+  const idExceptArray = id instanceof Array ? id[0] : id;
+  const data = await client.get({
+    endpoint: 'articles',
+    contentId: idExceptArray,
+  });
+
+  return {
+    props: {
+      article: data,
+    },
+  };
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,56 @@
+// pages/index.js
+import Link from "next/link";
+import type { Article } from '../../types/article';
+import { client } from "../../libs/client";
+
+type Props = {
+  articles: Array<Article>;
+}
+
+export default function Home({ articles }: Props) {
+  return (
+    <>
+      <h1 className="">
+        記事一覧
+      </h1>
+      <div className="">
+        {articles.map(article => (
+          <div className="" key={article.id}>
+            <img
+              className=""
+              src={article.eye_catch.url}
+              alt="Sunset in the mountains"
+            />
+            <div className="">
+              <Link href={`/article/${article.id}`} passHref>
+                <a>{article.title}</a>
+              </Link>
+            </div>
+            <div className="">
+              {article.tag && (
+                <span className="">
+                  #{article.tag}
+                </span>
+              )}
+            </div>
+            <div className="">
+              <p>{article.body}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "articles" });
+
+  return {
+    props: {
+      articles: data.contents,
+    },
+  };
+};
+
