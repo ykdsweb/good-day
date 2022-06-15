48c1039510510b4116c846d551d6f81f34d009e4
Tatsuya1117 <othe.world.pc@gmail.com>
2022-06-15 16:01:44 +0900

    toTopButtonを追加

--- src/components/layouts/ToTopButton.tsx
+++ src/components/layouts/ToTopButton.tsx
@@ -1,54 +1,59 @@
 // 押すとTopにスクロールするボタン
 import { useEffect, useState } from "react";
+import styles from "../../styles/ToTopButton.module.scss";
 
-export default function ReturnTopButton() {
-    const [isButtonActive, setIsButtonActive] = useState(false)
+export default function ToTopButton() {
+  const [isButtonActive, setIsButtonActive] = useState(false);
 
-    // 最終的に押したら動きをつけたいボタン
-    const returnTop= () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        })
+  // 最終的に押したら動きをつけたいボタン
+  const returnTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  // scrollした時の位置と動きの方向
+  const scrollWindow = () => {
+    const top = 640; // ボタンを表示させる位置
+    let scroll = 0;
+    scroll = window.scrollY;
+
+    if (top <= scroll) {
+      setIsButtonActive(true);
+    } else {
+      setIsButtonActive(false);
     }
+  };
 
-    // scrollした時の位置と動きの方向
-    const scrollWindow = () => {
-        const top = 100 // ボタンを表示させる位置
-        let scroll = 0
-        scroll = window.scrollY
+  // 止まっている時のスタイル
+  const normalStyle = {
+    opacity: 0,
+    transition: "0.4s",
+    pointerEvents: "none",
+  };
+  // 動いた時のスタイル
+  const activeStyle = {
+    opacity: 1,
+    transition: "0.4s",
+  };
+  // ボタンの状態を判定する
+  const style = isButtonActive ? activeStyle : normalStyle;
 
-        if (top <= scroll) {
-            setIsButtonActive(true)
-        } else {
-            setIsButtonActive(false)
-        }
-    }
+  useEffect(() => {
+    window.addEventListener("scroll", scrollWindow);
+    return () => {
+      window.removeEventListener("scroll", scrollWindow);
+    };
+  }, []);
 
-    // 止まっている時のスタイル
-    const normalStyle = {
-        opacity: 1,
-        transition: "0.5s",
-        pointerEvents: "none"
-    }
-    // 動いた時のスタイル
-    const activeStyle = {
-        opacity: 1,
-        transition: "0.5s"
-    }
-    // ボタンの状態を判定する
-    const style = isButtonActive ? activeStyle : normalStyle
-
-    useEffect(() => {
-        window.addEventListener("scroll", scrollWindow)
-        return () => {
-            window.removeEventListener("scroll", scrollWindow)
-        }
-    }, [])
-
-    return (
-        <button style={style} onClick={returnTop}>
-            ボタン
-        </button>
-    )
-  }
+  return (
+    <>
+      <button
+        className={styles.toTopButton}
+        style={style}
+        onClick={returnTop}
+      ></button>
+    </>
+  );
+}
