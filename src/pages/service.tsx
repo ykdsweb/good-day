1a935878ae59f48933218b2c3ae3c6e17b70221f
Tatsuya1117 <othe.world.pc@gmail.com>
2022-06-27 19:16:54 +0900

    404pageを追加

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -27,14 +27,6 @@ const Service: React.FC = () => {
   return (
     <>
       <Features />
-      <Features />
-      <Features />
-      <Features />
-      <Features />
-      <Features />
-      <Features />
-      <Features />
-      <Features />
     </>
   );
 };
