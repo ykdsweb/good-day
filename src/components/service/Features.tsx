9bc01cf99945ec871cda549976ada894a568b0c5
Tatsuya1117 <othe.world.pc@gmail.com>
2022-06-27 20:03:56 +0900

    Serviceページを作成

--- src/components/service/Features.tsx
+++ src/components/service/Features.tsx
@@ -1,12 +1,36 @@
+import Link from "next/link";
 import styles from "../../styles/Service.module.scss";
 
-export default function Features() {
+const Features: React.FC = () => {
+
   return (
     <>
       <div className={styles.features}>
-        <div className={styles.featuresTitle}>title</div>
-        <div className={styles.featuresDescription}>description</div>
+        <>
+          <h2 className={styles.featuresTitle}>title</h2>
+          <div className={styles.featuresSubTitle}>subtitle</div>
+          <div className={styles.featuresDescription}>description</div>
+        </>
+        <>
+          <h2 className={styles.featuresTitle}>title</h2>
+          <div className={styles.featuresSubTitle}>subtitle</div>
+          <div className={styles.featuresDescription}>description</div>
+        </>
+        <>
+          <h2 className={styles.featuresTitle}>title</h2>
+          <div className={styles.featuresSubTitle}>subtitle</div>
+          <div className={styles.featuresDescription}>description</div>
+        </>
+        <div className={styles.returnToTop}>
+          <Link href={"/"}>
+            <a>
+              <h2 className="">Topに戻る</h2>
+            </a>
+          </Link>
+        </div>
       </div>
     </>
   );
 }
+
+export default Features;
