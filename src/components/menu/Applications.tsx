eb974e736324858c1154196c51d608aec019dd35
Tatsuya1117 <othe.world.pc@gmail.com>
2022-06-29 16:55:47 +0900

    Recruitページを作成

---
+++ src/components/menu/Applications.tsx
@@ -0,0 +1,169 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../styles/StylistMenu/StylistMenu.module.scss";
+
+export default function Applications() {
+  return (
+    <div className={styles.menu}>
+      <table className={styles.menuArea}>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>募集要項(GOOD-DAY倉敷中島店)</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>雇用形態</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>募集職種・技術ランク</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>勤務先・アクセス</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>必須免許・資格</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>こだわり</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>報酬</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>休日</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>貸与・支給品</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>会費</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>メリット</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTablLast}>
+            <th className={styles.menuTableTh}>応募方法</th>
+            <td className={styles.menuTableTd}>
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+              テキスト。
+              <br />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/pages/recruit.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import Recruitment from "../components/recruit/Recruitment";
+
+export default function Recruit() {
+  return (
+    <>
+      <Recruitment />
+    </>
+  );
+}
