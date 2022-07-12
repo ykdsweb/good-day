e253f66bd418f8cd430b03af3a3222858afe5c49
Tatsuya1117 <othe.world.pc@gmail.com>
2022-07-12 13:42:50 +0900

    nextjs-progressbarを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import "nprogress/nprogress.css";
 import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
+import NextNprogress from "nextjs-progressbar";
 import Footer from "../components/layouts/Footer";
 import HamburgerMenu from "../components/layouts/HamburgerMenu";
 
@@ -20,6 +22,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta property="" content={"title"} />
         <meta property="" content={"description"} />
       </Head>
+      <NextNprogress
+        color="aqua"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={4}
+        showOnShallow={true}
+      />
       <HamburgerMenu />
       <Component {...pageProps} />
       <Footer />
