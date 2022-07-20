c3c5b3f7a58758a3a8384c345e0fda265f92ffe0
Tatsuya1117 <othe.world.pc@gmail.com>
2022-07-20 09:46:42 +0900

    cssを調節

--- src/components/home/TopCarousel.tsx
+++ src/components/home/TopCarousel.tsx
@@ -4,7 +4,7 @@ import { Carousel } from "react-responsive-carousel";
 import styles from "../../styles/Layouts/Carousel.module.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const ReactCarousel = () => {
+const TopCarousel = () => {
   const images: string[] = [
     "/media/main_slider_00.jpg",
     "/media/main_slider_01.jpg",
@@ -13,11 +13,12 @@ const ReactCarousel = () => {
   return (
     <>
       <Carousel
-        showArrows={true}
-        showThumbs={false}
         autoPlay={true}
         infiniteLoop={true}
         interval={4000}
+        showArrows={true}
+        showThumbs={false}
+        statusFormatter={() => ``}
       >
         {images.map((url, index) => (
           <div key={index}>
@@ -34,4 +35,4 @@ const ReactCarousel = () => {
   );
 };
 
-export default ReactCarousel;
+export default TopCarousel;
